4bd685f291f766e46e009c749ab16a1140ec9928
Manish singh <85191003+itsmanishsingh@users.noreply.github.com>
2023-03-09 18:52:57 +0000

    User Schema

--- app.js
+++ app.js
@@ -3,9 +3,28 @@ require("./database/database").connect()
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
 
 app.get("/", (req,res)=>{
     res.send(`<h1>Server is running successfully</h1>`)
+})
+
+app.post("/register" ,async(req,res)=>{
+    try {
+        // get all the data form body or || url
+        // all the data must be present
+        // check if User already register
+        // encrypt the password
+        // Save the user in DB
+        // Generate the token for user and send it 
+        
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 module.exports = app;
