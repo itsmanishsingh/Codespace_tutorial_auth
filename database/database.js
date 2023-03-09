82e79ac27d84aaa0aec1977361ba6aac903a16a2
Manish singh <85191003+itsmanishsingh@users.noreply.github.com>
2023-03-09 18:27:37 +0000

    DB connected

--- database/database.js
+++ database/database.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URL =process.env
+const {MONGODB_URL} =process.env
 
 exports.connect = ()=>{
+    console.log(typeof(MONGODB_URL));
     mongoose.connect(MONGODB_URL,{
         useNewUrlParser :true,
         useUnifiedTopology :true
     })
-    .then()
+    .then(
+        console.log(`DB connected Successfully`)
+    )
     .catch((error)=>{
         console.log(`DB Conneciton FAILED`);
         console.log(error);
